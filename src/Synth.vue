d7f11a34da9472ccd77ffba6775d3991249c6b34
ericpharr <ericjpharr@gmail.com>
2017-12-02 15:14:05 -0600

    initial commit. simple synth component.

---
+++ src/Synth.vue
@@ -0,0 +1,25 @@
+<template id="Synth" lang="html">
+	<button type="button" name="button" @click="playNote">Tone</button>
+</template>
+
+<script>
+import Tone from 'tone'
+
+export default {
+	name: 'synth',
+	data() {
+		return {
+			synth: new Tone.MonoSynth(),
+		}
+	},
+	methods: {
+		playNote() {
+			this.synth.toMaster();
+			this.synth.triggerAttackRelease("C4", "8n");
+		}
+	}
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Synth from './Synth.vue'
+// import Tone from 'tone'
+
+new Vue({
+  el: '#synth',
+  render: h => h(Synth)
+})
