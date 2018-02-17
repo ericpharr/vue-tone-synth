12eab53e9ddedaa7f641b72f59b22aa026b29001
ericpharr <ericjpharr@gmail.com>
2018-02-16 20:58:26 -0600

    Updated vuetify and added theming.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,17 @@ import VueCircleSlider from 'vue-circle-slider'
 import Vuetify from 'vuetify'
 
 Vue.use(VueCircleSlider)
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+    theme: {
+      primary: '#364554',
+      secondary: '#78be9a',
+      accent: '#eafab3',
+      error: '#FF5252',
+      info: '#d0d6ca',
+      success: '#4CAF50',
+      warning: '#FFC107'
+    }
+})
 
 new Vue({
   el: '#synth',
